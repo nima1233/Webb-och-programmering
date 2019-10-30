2304df3ab8540dcc7c95535b3372bffb0ba8d0c3
nima1233 <43337683+nima1233@users.noreply.github.com>
2019-10-30 13:46:21 +0100

    nodejs tutorial

---
+++ Niklas/Webbserver/w3node/demo_fs_replace.js
@@ -0,0 +1,7 @@
+var fs = require('fs');
+
+fs.writeFile('mynewfile3.txt', 'This is my text', function (err) {
+    if (err)
+    throw err;
+    console.log('Replaced!');
+});
---
+++ Niklas/Webbserver/w3node/demo_url.js
@@ -0,0 +1,10 @@
+var url = require('url');
+var adr = 'http://localhost:8080/default.htm?year=2017&month=february';
+var q = url.parse(adr, true);
+
+console.log(q.host);
+console.log(q.patchname);
+console.log(q.search);
+
+var qdata = q.query;
+console.log(q.data.month);
